fbdd748f170288b155e51604359a459394011559
Andrew Knight <knightandrew21@icloud.com>
2021-05-07 09:30:22 -0700

    basic outline

---
+++ code.js
@@ -0,0 +1,18 @@
+// Where to start with this app?
+
+// Let's try: add, subtract, multiply, divide
+
+const addTwoNumbers = (num1, num2) => {
+    const sumOfTwoNumbers = num1 + num2;
+    return sumOfTwoNumbers;
+}
+
+const subtractTwoNumbers = (num1, num2) => {
+    const differenceOfTwoNumbers = num1 - num2;
+    return differenceOfTwoNumbers;
+}
+
+const multiplyTwoNumbers = (num1, num2) => {
+    const 
+}
+console.log(subtractTwoNumbers(103,-25));
