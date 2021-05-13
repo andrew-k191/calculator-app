c0e01ff443b543263bdb25a0b5cc87c90bc22eed
Andrew Knight <knightandrew21@icloud.com>
2021-05-13 13:49:25 -0700

    almost there, decimal working

--- code.js
+++ code.js
@@ -1,173 +1,158 @@
-const calculatorDisplay = document.querySelector('.calculatorDisplay');
-
+const calculatorDisplay = document.querySelector('.calculator-display');
+// Initialize variables that will be useful
 let firstOperand = '';
-let secondOperand = '';
-let selectedOperator = '';
+let secondOperand = ''; 
+let arithmeticOperator = ''; 
+// let canAddDecimal = true;
+let currentResult = 0; // Should always remain a number
 
-// EventListeners
+// Conditions that can initialize 1st operand:
+// * arithmetic operator must not exist 
+// * secondOperand must not exist 
+// * currentResult can be reassigned to firstOperand
 
-/* numbers generator */
-document.querySelectorAll('.numbers')
-.forEach(numberButton => {
+// Conditions that can initialize arithmetic operator(s):
+// * firstOperand must exist or current result must exist
+
+// Conditions that can initialize 2nd operand:
+// * firstOperand must exist
+// * arithmetic operator must exist
+
+
+// Conditions solver can be invoked
+// * firstOperand, arithmetic operator, secondOperand exists
+
+/* Numbers Generator */
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', (e) => {
-        if (!selectedOperator) {
+        if (!arithmeticOperator) {
             firstOperand += e.target.textContent;
             calculatorDisplay.textContent = firstOperand;
-            // console.log('firstOperand: ', firstOperand);
+            console.log('firstOperand: ', firstOperand);
         }
-        if (firstOperand && selectedOperator) {
+        if (firstOperand && arithmeticOperator) {
             secondOperand += e.target.textContent;
             calculatorDisplay.textContent = secondOperand;
-            // console.log('secondOperand: ', secondOperand);
+            console.log('secondOperand: ', secondOperand);
         }
     });
 });
 
-/* arithmetic operator identifier */
-document.querySelectorAll('.operators')
-.forEach(operatorButton => {
-    operatorButton.addEventListener('click', (e) => {
-        if (firstOperand) {
-            selectedOperator = e.target.textContent;
-            // console.log('selectedOperator: ', selectedOperator);
-        }
-    });
-});
-
-/* equation solver */
-document.querySelector('.equate')
-.addEventListener('click', () => {
-    switch (selectedOperator) {
-        case '+':
-            calculatorDisplay.textContent = parseInt(firstOperand) + parseInt(secondOperand);
-            // Resetting Values 
-            firstOperand = '';
-            secondOperand = '';
-            selectedOperator = '';
-            break;
+const addDecimalToOperand = (operand) => {
+    const operandArray = operand.split('');
+    if (operandArray.indexOf('.') === -1) {
+        operandArray.push('.');
     }
+    return operandArray.join('');
+};
+
+/* Decimal Button */
+// can add decimal only once for firstOperand and secondOperand 
+const decimalButton = document.querySelector('.decimal');
+decimalButton.addEventListener('click', () => {
+    if (!arithmeticOperator) {
+        firstOperand = addDecimalToOperand(firstOperand);
+        calculatorDisplay.textContent = firstOperand;
+        // firstOperand += e.target.textContent;
+        // calculatorDisplay.textContent += e.target.textContent;
+        // canAddDecimal = false;
+    }
+    else if (firstOperand && arithmeticOperator) {
+        secondOperand = addDecimalToOperand(secondOperand);
+        calculatorDisplay.textContent = secondOperand;
+        // secondOperand += e.target.textContent;
+        // calculatorDisplay.textContent += e.target.textContent;
+        // canAddDecimal = false;
+    }
+    // && canAddDecimal
 });
 
-/* clear display */
-document.querySelector('.clearDisplay')
-.addEventListener('click', () => {
+/* Arithmetic Operator */
+const arithmeticOperatorButtons = document.querySelectorAll('.arithmetic-operator')
+arithmeticOperatorButtons.forEach(arithmeticOperatorButton => {
+    arithmeticOperatorButton.addEventListener('click', (e) => {
+        if (firstOperand) {
+            arithmeticOperator = e.target.textContent;
+            // canAddDecimal = true;
+            console.log('arithmeticOperator: ', arithmeticOperator);
+        } 
+        // if firstOperand doesn't exist and arithmetic operator is invoked
+        else if (!firstOperand) {
+            firstOperand = currentResult;
+            arithmeticOperator = e.target.textContent;
+            // canAddDecimal = true; 
+            console.log('arithmeticOperator: ', arithmeticOperator);
+        }
+    });
+});
+
+/* Equation Solver */
+const equationSolver = document.querySelector('.equation-solver');
+equationSolver.addEventListener('click', () => {
+    firstOperand = parseFloat(firstOperand);
+    if (firstOperand && secondOperand) {
+        secondOperand = parseFloat(secondOperand);
+        switch (arithmeticOperator) {
+            case '+':
+                currentResult = firstOperand + secondOperand;
+                console.log(currentResult);
+                break;
+            case '-':
+                currentResult = firstOperand - secondOperand;
+                break;
+            case '×':
+                currentResult = firstOperand * secondOperand;
+                break;
+            case '÷':
+                currentResult = firstOperand / secondOperand;
+                break;
+        } 
+        // Continuous Functionality
+        currentResult = parseFloat(currentResult.toFixed(3));
+        console.log('CurrentResult: ', currentResult);
+        calculatorDisplay.textContent = currentResult;
+        firstOperand = secondOperand = arithmeticOperator = '';
+        // canAddDecimal = true; 
+    } 
+    // // currentResult, arithmeticOperation, firstOperand
+    // // second operand does not exist
+    // else if (firstOperand && !secondOperand) {
+    //     switch (arithmeticOperator) {
+    //         case '+':
+    //             currentResult += firstOperand;
+    //             break;
+    //         case '-':
+    //             currentResult -= firstOperand;
+    //             break;
+    //         case '×':
+    //             currentResult *= firstOperand;
+    //             break;
+    //         case '÷':
+    //             currentResult /= firstOperand;
+    //             break;
+    //     } 
+    //     // Continuous Functionality
+    //     currentResult = parseFloat(currentResult.toFixed(3));
+    //     console.log('CurrentResult: ', currentResult);
+    //     calculatorDisplay.textContent = currentResult;
+    //     firstOperand = secondOperand = arithmeticOperator = '';
+    //     canAddDecimal = true; 
+    // }
+});
+
+/* Clear All Button */
+const clearDisplayButton = document.querySelector('.clear-display');
+clearDisplayButton.addEventListener('click', () => {
+    // Resets all values 
     calculatorDisplay.textContent = 0;
-    firstOperand = '';
-    secondOperand = '';
-    selectedOperator = '';
+    firstOperand = secondOperand = arithmeticOperator = '';
+    currentResult = 0;
+    // canAddDecimal = true;
 });
 
 
+// check if number has at least one decimal
+// if so, decimals are not allowed
+// otherwise decimals are allowed
 
-
-
-
-
-
-
-
-
-
-
-
-
-// let canResetCalculatorDisplay = 'NO';
-// let calculatorValue = 0;
-
-// /* clear button */
-// document.querySelector('.clearButton')
-// .addEventListener('click', () => {
-//     calculatorDisplay.textContent = 0;
-// });
-
-
-// // /* when + button is clicked */
-// document.querySelector('.addButton')
-// .addEventListener('click', () => {
-//     calculatorValue += parseInt(calculatorDisplay.textContent);
-//     calculatorDisplay.textContent = calculatorValue;
-//     canResetCalculatorDisplay = 'YES';
-// });
-
-// /* number one button */
-// document.querySelector('.oneButton')
-// .addEventListener('click', () => {
-//     if ((parseInt(calculatorDisplay.textContent) === 0) || (canResetCalculatorDisplay === 'YES')) {
-//         calculatorDisplay.textContent = 1;
-//         canResetCalculatorDisplay = 'NO';
-//     } else {
-//         calculatorDisplay.textContent += 1;
-//     }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /* arithmetic operator */
-// let arithmeticOperator = null;
-
-// const numberTrackerBeforeOperation = (captureCalculatorDisplayNumber) => {
-//     const numberBefore = parseInt(captureCalculatorDisplayNumber.textContent);
-//     return numberBefore;
-// };
-
-// const numberTrackerAfterOperation = (captureCalculatorDisplayNumber) => {
-//     const numberAfter = parseInt(captureCalculatorDisplayNumber.textContent);
-//     return numberAfter; 
-// };
-
-
-
-/* when = button is clicked */
-// document.querySelector('.equalButton')
-// .addEventListener('click', () => {
-
-// })
-
-
-
-
-/* Calculator Numbers */
-// const zeroButton = document.querySelector('.zeroButton');
-// const oneButton = document.querySelector('.oneButton');
-// const twoButton = document.querySelector('.twoButton');
-// const threeButton = document.querySelector('.threeButton');
-// const fourButton = document.querySelector('.fourButton');
-// const fiveButton = document.querySelector('.fiveButton');
-// const sixButton = document.querySelector('.sixButton');
-// const sevenButton = document.querySelector('.sevenButton');
-// const eightButton = document.querySelector('.eightButton');
-// const nineButton = document.querySelector('.nineButton');
-
-// const zeroButton = () => {
-//     document.querySelector('.zeroButton')
-//     .addEventListener('click', () => {
-//         calculatorDisplay.textContent = 0;
-//     })
-// };
